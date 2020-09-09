af98e2c40befac72aae5ea221fd60de25d7d2d6e
hworks <gene.hartsell@gmail.com>
2020-09-09 05:50:10 -0700

    Image path bug.

--- dore-ssg/docgen.go
+++ dore-ssg/docgen.go
@@ -409,6 +409,9 @@ func navGen() {
 		        if tokenType == html.SelfClosingTagToken {
 		        	if itoken := tokenizer.Token(); itoken.Data == "img" {
 		        		imgPath := itoken.Attr[0].Val
+		        		if (len(itoken.Attr) > 3) {
+		        			imgPath = itoken.Attr[2].Val
+		        		}
 						if err := copyImg("src/" + imgPath, "pub/" + imgPath); err != nil {
 							fmt.Println(err.Error())
 						}
